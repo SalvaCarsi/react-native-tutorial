c74820555e337695db66dcdc4e8f6723fc8d8bcd
SalvaCarsi <salvavcarsi@gmail.com>
2017-01-12 16:25:18 +0100

    height and width

--- index.ios.js
+++ index.ios.js
@@ -20,11 +20,15 @@ export default class reactnative extends Component {
     };
     const loremIpsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     return (
-      <View>
-        <Text style={styles.welcome} numberOfLines={1} ellipsizeMode={'head'}>
-          `Welcome to React! ${loremIpsumText}` 
-        </Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} >
+          <Text style={styles.welcome} numberOfLines={1} ellipsizeMode={'head'}>
+            `Welcome to React! ${loremIpsumText}` 
+          </Text>
+          <Image source={pic} style={{width: 193, height: 110}}/>
+        </View>
       </View>
     );
   }
